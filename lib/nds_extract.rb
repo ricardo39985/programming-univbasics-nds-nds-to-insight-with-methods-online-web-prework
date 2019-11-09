3f4ad90a6029029ea799524eedba533663134c31
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-09 23:04:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,5 +20,9 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  pp director_data
+  total = 0
+  for i in director_data[:movies] do
+    total += i[:worldwide_gross]
+  end
+  total
 end
